18dc343506b30fc0adc2a0cd910f720a5e715f83
rorr <rlayneorr@aim.com>
2018-02-06 14:44:07 -0500

    adding a servlet

---
+++ PipelineDemo/src/main/java/com/revature/servlets/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		resp.getWriter().write("<h1>HELLO WORLD</h1>");
+	}
+}
